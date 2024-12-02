9cb69e42412d894ba16354e5a7c9481291e3f65f
Christian Bouwense <cbouwense@proton.me>
2024-12-01 20:56:13 -0500

    did part two of day one

--- c/1/main.c
+++ c/1/main.c
@@ -1,6 +1,6 @@
 #include "../guppy.h"
 
-void part_one(int argc, char **argv) {
+int part_one(int argc, char **argv) {
     int part_one_answer = 0;
     GupArena a = gup_arena_create();
 
@@ -42,12 +42,58 @@ void part_one(int argc, char **argv) {
         }
     }
 
-    printf("part_one_answer: %d\n", part_one_answer);
-    
     gup_arena_destroy(&a);
+
+    return part_one_answer;
+}
+
+int part_two(int argc, char **argv) {
+    int part_two_answer = 0;
+    GupArena a = gup_arena_create();
+
+    // example: [
+    //   "3   4",
+    //   "4   3",
+    //   "2   5",
+    // ]
+    const char *file_path = argc > 1 ? argv[1] : "input.txt";
+
+    GupArrayString file_lines = gup_file_read_lines_arena(&a, file_path);
+    GupArrayInt left_ints = gup_array_int_create_arena(&a);
+    GupArrayInt right_ints = gup_array_int_create_arena(&a);
+
+    for (int i = 0; i < file_lines.count; i++) {
+        const GupString line = file_lines.data[i];
+        
+        // "3   4" -> ["3", "4"]
+        GupArrayString tokens = gup_string_split_by_cstr_arena(&a, line, "   ");
+        int left_int, right_int = 0;
+        
+        gup_assert_verbose(gup_string_to_int(tokens.data[0], &left_int), "FATAL ERROR: Unable to parse integer out of the left number in the input line. You should probably print out the tokens and inspect if there's any sort of corruption or weirdo input.");
+        gup_assert_verbose(gup_string_to_int(tokens.data[1], &right_int), "FATAL ERROR: Unable to parse integer out of the right number in the input line. You should probably print out the tokens and inspect if there's any sort of corruption or weirdo input.");
+
+        gup_array_int_append_arena(&a, &left_ints, left_int);
+        gup_array_int_append_arena(&a, &right_ints, right_int);
+    }
+    
+    for (int i = 0; i < left_ints.count; i++) {
+        int right_occurrences = 0;
+        for (int j = 0; j < right_ints.count; j++) {
+            if (left_ints.data[i] == right_ints.data[j]) {
+                right_occurrences++;
+            }
+        }
+        part_two_answer += left_ints.data[i] * right_occurrences;
+    }
+
+    gup_arena_destroy(&a);
+
+    return part_two_answer;
 }
 
 int main(int argc, char **argv) {
-    part_one(argc, argv);
+    printf("part 1: %d\n", part_one(argc, argv));
+    printf("part 2: %d\n", part_two(argc, argv));
+
     return 0;
 }
